57d4e65f8432b3c7c8a7def0b4125b164e4faddd
phamlam24 <ptlam61004@gmail.com>
2024-10-20 07:03:14 -0700

    socket test screen

---
+++ teachxr/src/SocketTestScreen.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const SocketTestScreen = () => {
+    const [message, setMessage] = useState('');
+    const [response, setResponse] = useState('');
+
+    useEffect(() => {
+        const socket = io('http://localhost:5000');
+
+        socket.on('connect', () => {
+            console.log('Connected to socket server');
+        });
+
+        socket.on('message', (data) => {
+            setResponse(data);
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    const sendMessage = () => {
+        const socket = io('http://localhost:5000');
+        socket.emit('message', message);
+    };
+
+    return (
+        <div>
+            <h1>Socket Test Screen</h1>
+            <input 
+                type="text" 
+                value={message} 
+                onChange={(e) => setMessage(e.target.value)} 
+                placeholder="Type a message" 
+            />
+            <button onClick={sendMessage}>Send Message</button>
+            <p>Response from server: {response}</p>
+        </div>
+    );
+};
+
+export default SocketTestScreen;
